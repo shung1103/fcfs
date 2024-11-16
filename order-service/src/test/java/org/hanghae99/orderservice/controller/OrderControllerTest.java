9d98024f5dc4bdb073343212ff1e07626b0619c8
shung1103 <shung1103@hanmail.net>
2024-11-16 12:31:35 +0900

    [FIX] Test START 2

--- order-service/src/test/java/org/hanghae99/orderservice/controller/OrderControllerTest.java
+++ order-service/src/test/java/org/hanghae99/orderservice/controller/OrderControllerTest.java
@@ -1,4 +1,54 @@
+package org.hanghae99.orderservice.controller;
+
+import org.hanghae99.orderservice.service.OrderService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(OrderController.class)
 class OrderControllerTest {
-  
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    OrderService orderService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createOrder() {
+    }
+
+    @Test
+    void getMyOrders() {
+    }
+
+    @Test
+    void getOneOrder() {
+    }
+
+    @Test
+    void cancelOrder() {
+    }
+
+    @Test
+    void completeOrder() {
+    }
+
+    @Test
+    void adaptGetOrders() {
+    }
 }
